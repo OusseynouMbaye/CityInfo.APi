5f3d7413e468999ef34bbd8824faaa9d610bfd26
Ouzinem <ouzinem@hotmail.com>
2022-07-19 20:19:36 -0400

    create citiController

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, name = "New York" },
+                    new {id=2, name = "Chicago"}
+                });
+        }
+    }
+}
