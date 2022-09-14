13991a1b685b210da2775ac3b09f0e4ddf1135ad
Ouzinem <ouzinem@hotmail.com>
2022-09-14 09:18:17 -0400

    Working with Migrations

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -13,9 +13,12 @@ namespace CityInfo.API.Entities
         [MaxLength(50)]
         public string Name { get; set; }
 
+        [MaxLength(200)]
+        public string Description { get; set; }
+
         [ForeignKey("CityId")]
         public City? City { get; set; }
-        
+
         // key etrangere
         public int CityId { get; set; }
 
