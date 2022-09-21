c2580586c1cf38b11f005a9e6bd0c544f0822e68
Ouzinem <ouzinem@hotmail.com>
2022-09-21 10:43:33 -0400

    implement repository pattern

---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,45 @@
+﻿using CityInfo.API.DbContexts;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            return await _context.Cities.OrderBy(citie => citie.Name).ToListAsync();
+        }
+
+        public async Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest)
+        {
+            if (includePointsOfInterest)
+            {
+                return await _context.Cities.Include(citie => citie.PointsOfInterest)
+                    .Where(citie => citie.Id == cityId).FirstOrDefaultAsync();
+            }
+
+            return await _context.Cities.Where(citie => citie.Id == cityId).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId)
+        {
+            return await _context.PointOfInterest.Where(point => point.CityId == cityId).ToListAsync();
+        }
+
+        public async  Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId)
+        {
+            return await _context.PointOfInterest.
+                 Where(point => point.CityId == cityId && point.Id == pointOfInterestId).FirstOrDefaultAsync();
+        }
+
+      
+    }
+}
