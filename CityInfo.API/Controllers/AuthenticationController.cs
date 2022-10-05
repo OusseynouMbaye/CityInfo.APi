5eada8df3def9beef2ecd1a341fb79472e7d81fc
Ouzinem <ouzinem@hotmail.com>
2022-10-05 10:37:35 -0400

    Creating a token

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")] //[Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        // i don't use this outside of this classe , so we can scope it to this namespace
+        public class AuthenticationRequestbody
+        {
+            public string? Username { get; set; }
+            public string? Password { get; set; }
+        }
+
+        public class CityinfoUser
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+
+            public CityinfoUser(
+                int userId, string userName, string firstName,
+                string lastName, string city)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+            }
+        }
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(
+            AuthenticationRequestbody authenticationRequestbody)
+        {
+            // Step 1: validate the username/password
+            var user = ValidateUserCredentials(
+                authenticationRequestbody.Username,
+                authenticationRequestbody.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            // Step 2:  create a token
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256);
+
+            // The claims that
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", user.UserId.ToString()));
+            claimsForToken.Add(new Claim("given_name", user.FirstName));
+            claimsForToken.Add(new Claim("family_name", user.LastName));
+            claimsForToken.Add(new Claim("city", user.City));
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials);
+
+            var tokenToReturn = new JwtSecurityTokenHandler()
+                .WriteToken(jwtSecurityToken);
+
+            return Ok(tokenToReturn);
+        }
+
+        private CityinfoUser ValidateUserCredentials(string? username, string? password)
+        {
+            // return a new CityInfoUser (values would normally come from your user DB/table)
+            return new CityinfoUser(
+                1,
+                username ?? "",
+                "Ouzin",
+                "Mbaye",
+                "Antwerp");
+        }
+    }
+}
