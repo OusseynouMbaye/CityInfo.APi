232287a42690d49a21db4db4ff2a7455fe43841c
Ouzinem <ouzinem@hotmail.com>
2022-10-07 10:44:37 -0400

    Documenting api with comment ///

--- CityInfo.API/Models/CityWithoutPointOfInterestDto.cs
+++ CityInfo.API/Models/CityWithoutPointOfInterestDto.cs
@@ -1,9 +1,23 @@
 ﻿namespace CityInfo.API.Models
 {
+    /// <summary>
+    /// A DTO a city without points of interest
+    /// </summary>
     public class CityWithoutPointOfInterestDto
     {
+        /// <summary>
+        /// The id of city
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// The name of city
+        /// </summary>
         public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The description of city
+        /// </summary>
         public string? Description { get; set; }
     }
 }
